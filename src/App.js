30fac21890b2387aae3c77e0404798249150c254
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-10-18 15:28:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./components/Card";
+import data from "./helper/data";
+
+function App() {
+
+
+  return (
+
+    <Card  data={data} />
+
+  )
+
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Card = (props) => {
+
+
+    const [count, setCount] = useState(0)
+
+    const handleInc = () => {
+        if(count < 15){
+            setCount(count+5)
+        }else if(count+5 >= 15){
+            setCount(0)
+        }
+        
+    }
+
+    const handleDec = () => {
+        if(count > 0 ){
+            setCount(count-5)
+        }else if(count <= 0){
+            setCount(15)
+        }
+    }
+
+    console.log(count);
+    
+  return (
+    <div className='container text-center text-danger my-3'>
+        <h1>Employee List</h1>
+        <div className="col my-5">
+        <button onClick={handleDec} className='btn btn-primary me-5'>Previous</button>
+        <button onClick={handleInc} className='btn btn-primary'>Next</button>
+        </div>
+        {props.data.slice(count,count+5).map((item) => {
+            return (
+                <>
+                <h6>{item.name}</h6>
+                <img src={item.image} alt="" />
+                </>
+                
+            )
+        })}
+
+      
+    </div>
+  )
+}
+
+export default Card
